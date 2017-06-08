cb9804c1ce1f50dc1e431799fbc1e61f3df9f956
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-06-08 16:08:09 -0500

    Creando metodos del Servicio

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -3,6 +3,31 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UserService {
 
+  userName: string;
+  userAge: number;
+  userEmail: string;
+  userFeeling: any;
+
   constructor() { }
+
+  // setters
+
+  setUserName(name: string): void{
+    this.userName = name; // asignamos nombre al atributo.
+  }
+
+  setUserAge(age:number): void{
+    this.userAge = age; // asignamos edad al atributo.
+  }
+
+  // getters
+
+  getUserName(): string{
+    return this.userName;
+  }
+
+  getUserNumber(): number{
+    return this.userAge;
+  }
 
 }
